185f68c5cbf406c1b6485962f478b26c80755369
Ivana Sparic <sparicivana.biz@gmail.com>
2023-09-25 16:01:30 +0200

    Set html layout

--- app.vue
+++ app.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
-    <h1>Test</h1>
+    <TheHeader />
+
+    <main>
+      <!-- Product Gallery -->
+      <div>
+        <!-- Main image -->
+        <div></div>
+        <!-- Thumbnails -->
+        <div></div>
+      </div>
+
+      <!-- Product Description -->
+      <div>
+        <p>Sneaker company</p>
+        <h1>Fall Limited Edition Sneakers</h1>
+        <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole,
+          they’ll withstand everything the weather can offer. </p>
+        <!-- Price wrapper -->
+        <div>
+          <div>
+            <p>$125.00</p>
+            <p>50%</p>
+          </div>
+          <p>$250.00</p>
+        </div>
+        <!-- Button wrapper -->
+        <div>
+          <button>0</button>
+          <button>
+            <img
+              src="../images/icon-cart.svg"
+              alt="Add to cart"
+            >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </main>
+
   </div>
 </template>
---
+++ components/TheHeader.vue
@@ -0,0 +1,80 @@
+
+<template>
+    <header class="flex justify-content-space-between">
+        <!-- Header - LEFT -->
+        <div class="flex">
+            <!-- Hamburger -->
+            <button
+                type="button"
+                aria-haspopup="true"
+                aria-controls="main-nav"
+                aria-expanded="false"
+                class="hamburger-bar"
+            >
+
+                <img
+                    src="../images/icon-menu.svg"
+                    alt="Open menu button"
+                >
+            </button>
+
+            <!-- Logo -->
+            <img
+                src="../images/logo.svg"
+                alt="Open menu button"
+            >
+            <!-- Navigation -->
+            <nav
+                class="nav"
+                id="main-nav"
+                aria-label="Main navigation"
+            >
+                <ul>
+                    <li><a href="#">Collections</a></li>
+                    <li><a href="#">Men</a></li>
+                    <li><a href="#">Women</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+        <!-- Header - RIGHT -->
+        <div class="flex">
+            <!-- Add to Cart button -->
+            <button>
+                <img
+                    src="../images/icon-cart.svg"
+                    alt="Add to cart"
+                >
+            </button>
+
+            <!-- Profile avatar -->
+            <img
+                src="../images/image-avatar.png"
+                alt="Profile avatar"
+                class="header__avatar"
+            >
+        </div>
+
+    </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.nav {
+    display: none;
+}
+
+.header__avatar {
+    width: 55px;
+    padding: 1rem;
+}
+
+.hamburger-bar {
+    padding: 1rem
+}
+</style>
