5cbd565e1b22843ae9b9d785a6c56db2bc28acf5
Ivana Sparic <sparicivana.biz@gmail.com>
2023-10-24 18:05:52 +0200

    Create MainNavigation component

---
+++ components/MainNavigation.vue
@@ -0,0 +1,100 @@
+
+<template>
+    <nav
+        class="nav"
+        id="main-nav"
+        aria-label="Main navigation"
+    >
+        <button
+            @click="$emit('closeMenu')"
+            class="nav__close-btn"
+        >
+            <img
+                src="/images/icon-close.svg"
+                alt="Close menu"
+            >
+        </button>
+        <ul class="nav__list margin-inline-start-600">
+            <li><a
+                    href="#"
+                    class="nav__link"
+                >Collections</a></li>
+            <li><a
+                    href="#"
+                    class="nav__link"
+                >Men</a></li>
+            <li><a
+                    href="#"
+                    class="nav__link"
+                >Women</a></li>
+            <li><a
+                    href="#"
+                    class="nav__link"
+                >About</a></li>
+            <li><a
+                    href="#"
+                    class="nav__link"
+                >Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.nav__close-btn {
+    display: none;
+}
+
+@media screen and (max-width: 768px) {
+
+    .nav {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 70vw;
+
+        background-color: var(--white);
+    }
+
+    .nav__close-btn {
+        display: block;
+        padding: var(--spacing-600);
+    }
+
+    .nav__link {
+        display: inline-block;
+        padding-inline: var(--spacing-600);
+        padding-block: var(--spacing-200);
+        color: var(--very-dark-blue);
+        font-weight: var(--font-weight-bold);
+    }
+
+}
+
+@media screen and (min-width: 769px) {
+
+    .nav__list {
+        display: flex;
+        gap: var(--spacing-600)
+    }
+
+    .nav__link {
+        color: var(--dark-grayish-blue);
+        padding-block: var(--spacing-600);
+        display: inline-block;
+    }
+
+    .nav__link:hover {
+        color: var(--very-dark-blue);
+        padding-block-end: 21px;
+        padding-block-start: var(--spacing-600);
+        border-bottom: 3px solid var(--orange);
+    }
+
+
+}
+</style>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,8 +1,8 @@
 
 <template>
-    <header class="header flex justify-content-space-between padding-block-end-200">
+    <header class="header flex justify-content-space-between">
         <!-- Header - LEFT -->
-        <div class="flex">
+        <div class="flex align-self-flex-end">
             <!-- Hamburger -->
             <button
                 type="button"
@@ -29,44 +29,7 @@
                 class="nav-container"
                 :class="{ 'open': isMenuOpen }"
             >
-                <nav
-                    class="nav"
-                    id="main-nav"
-                    aria-label="Main navigation"
-                >
-                    <button
-                        @click="closeMenu"
-                        class="nav__close-btn"
-                    >
-                        <img
-                            src="/images/icon-close.svg"
-                            alt="Close menu"
-                        >
-                    </button>
-                    <ul class="nav__list margin-inline-start-600">
-                        <li><a
-                                href="#"
-                                class="nav__link"
-                            >Collections</a></li>
-                        <li><a
-                                href="#"
-                                class="nav__link"
-                            >Men</a></li>
-                        <li><a
-                                href="#"
-                                class="nav__link"
-                            >Women</a></li>
-                        <li><a
-                                href="#"
-                                class="nav__link"
-                            >About</a></li>
-                        <li><a
-                                href="#"
-                                class="nav__link"
-                            >Contact</a></li>
-                    </ul>
-                </nav>
-
+                <MainNavigation @close-menu="closeMenu" />
             </div>
 
 
@@ -98,11 +61,9 @@
 let isMenuOpen = ref(false)
 
 // open and close menu for small resolutions
-const openMenu = () => {
-    console.log("clicked")
-    isMenuOpen.value = true
-}
+const openMenu = () => isMenuOpen.value = true
 const closeMenu = () => isMenuOpen.value = false
+
 </script>
 
 <style scoped>
@@ -114,28 +75,11 @@ const closeMenu = () => isMenuOpen.value = false
     background-color: var(--white);
 }
 
-
-
 .hamburger-bar {
     display: none;
 }
 
-.nav__close-btn {
-    display: none;
-}
-
-
 @media screen and (max-width: 768px) {
-
-    .header__avatar {
-        width: 55px;
-        padding: var(--spacing-400);
-    }
-
-    .hamburger-bar {
-        display: block;
-        padding: var(--spacing-400);
-    }
 
     .nav-container {
         display: none;
@@ -148,50 +92,26 @@ const closeMenu = () => isMenuOpen.value = false
         background-color: var(--background);
     }
 
-    .nav {
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 70vw;
-
-        background-color: var(--white);
+    .header__avatar {
+        width: 55px;
+        padding: var(--spacing-400);
     }
 
-    .nav__close-btn {
+    .hamburger-bar {
         display: block;
-        padding: var(--spacing-600);
-    }
-
-    .nav__link {
-        display: inline-block;
-        padding-inline: var(--spacing-600);
-        padding-block: var(--spacing-200);
-        color: var(--very-dark-blue);
-        font-weight: var(--font-weight-bold);
+        padding: var(--spacing-400);
     }
 
 }
 
 @media screen and (min-width: 769px) {
-
     .header {
-        padding-block: var(--spacing-600);
         border-bottom: 1px solid #ebedf2;
     }
 
     .header__avatar {
         width: 75px;
         padding: var(--spacing-400);
-    }
-
-    .nav__list {
-        display: flex;
-        gap: var(--spacing-600)
-    }
-
-    .nav__link {
-        color: var(--dark-grayish-blue);
     }
 
     .header__add-to-cart-btn {
